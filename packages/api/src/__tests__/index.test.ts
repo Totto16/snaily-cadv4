aba466c0c98f6af01992e84e569c6f2ce0642e05
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-02-01 19:55:49 +0100

    :tada: add more tests

--- packages/api/src/__tests__/index.test.ts
+++ packages/api/src/__tests__/index.test.ts
@@ -1,3 +1,4 @@
+import { prisma } from "#lib/prisma";
 import { PlatformTest } from "@tsed/common";
 import SuperTest from "supertest";
 import { Server } from "../server";
@@ -10,13 +11,52 @@ describe("Authentication", () => {
   beforeAll(() => {
     request = SuperTest(PlatformTest.callback());
   });
+  beforeAll(async () => {
+    await prisma.cad.deleteMany();
+    await prisma.user.deleteMany();
+  });
   afterAll(PlatformTest.reset);
 
-  describe("POST /auth/login", () => {
-    it("should do something", async () => {
-      const response = await request.get("/rest/calendars").expect(200);
+  describe("GET /", () => {
+    it("Should return 200 response with 200 OK text", async () => {
+      const response = await request.get("/").expect(200);
+      expect(response.text).toEqual(
+        "<html><head><title>SnailyCAD API</title></head><body>200 Success</body></html>",
+      );
+    });
+  });
 
-      expect(typeof response.body).toEqual("array");
+  describe("POST /auth/login", () => {
+    it("Should return an error array when no values passed", async () => {
+      const response = await request.post("/v1/auth/login").expect(400);
+      expect(response.body.errors).toEqual([{ username: "Required", password: "Required" }]);
+    });
+
+    it("Should return an error when user not found", async () => {
+      const response = await request
+        .post("/v1/auth/login")
+        .send({ username: "CasperTheGhost", password: "@@@@@@@@@@@@" })
+        .expect(404);
+
+      expect(response.body.errors).toEqual([{ username: "userNotFound" }]);
+    });
+  });
+
+  describe("POST /auth/register", () => {
+    it("Should return an error array when no values passed", async () => {
+      const response = await request.post("/v1/auth/register").expect(400);
+      expect(response.body.errors).toEqual([{ username: "Required", password: "Required" }]);
+    });
+
+    it("Should return the created userId and isOwner properties", async () => {
+      const response = await request
+        .post("/v1/auth/register")
+        .send({ username: "CasperTheGhost", password: "@@@@@@@@@@@@" })
+        .expect(200);
+
+      // first account is owner
+      expect(response.body.userId).toMatch(/([a-z])\w+/gi);
+      expect(response.body.isOwner).toBeTruthy();
     });
   });
 });
